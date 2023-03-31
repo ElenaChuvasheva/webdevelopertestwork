ecbe2370bc44eda73b4416952a396f31650de2d8
Елена Чувашева <chuvashevalena@yandex.ru>
2023-03-31 15:42:43 +0300

    dockerignore на место

--- server/migrations/versions/7e8364c20c54_orders.py
+++ server/migrations/versions/7e8364c20c54_orders.py
@@ -5,8 +5,8 @@ Revises:
 Create Date: 2023-03-29 14:27:28.957135
 
 """
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
